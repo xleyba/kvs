d97e238a1867d34b92d546bacc0392b6f597f667
Javier Leyba <135663+xleyba@users.noreply.github.com>
2020-09-02 09:57:26 +0200

    Added hashmap, tests passed

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,26 @@
+use std::collections::HashMap;
 
-pub struct KvStore{}
+pub struct KvStore{
+    db: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-    panic!()
+        KvStore {
+            db: HashMap::new(),
+        }
     }
 
-    pub fn set(&mut self, key: String, value: String) {}
+    pub fn set(&mut self, key: String, value: String) {
+        self.db.insert(key, value);
+    }
 
     pub fn get(&mut self, key: String) -> Option<String> {
-        panic!()
+        self.db.get(&key).cloned()
     }
 
     pub fn remove(&mut self, key: String) {
+        self.db.remove(&key);
     }
 
 }
