903cbae08d75bcfc2a8b94a4f39f6a32012499ad
Javier Leyba <135663+xleyba@users.noreply.github.com>
2020-09-02 15:00:14 +0200

    Refactored to structopt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,68 +1,49 @@
 extern crate clap;
-use clap::{App, AppSettings, Arg, SubCommand};
 use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = env!("CARGO_PKG_NAME"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    version = env!("CARGO_PKG_VERSION")
+    )]
+enum Opt {
+    #[structopt(name = "set", about = "store a value for a key")]
+    Set {
+        #[structopt(value_name = "KEY", help = "The key to insert.")]
+        key: String,
+        #[structopt(value_name = "VALUE", help = "The value to insert.")]
+        value: String,
+    },
+    #[structopt(name = "get", about = "get a value for a given key")]
+    Get {
+        #[structopt(value_name = "KEY", help = "The key to get the value")]
+        key: String,
+    },
+    #[structopt(name = "rm")]
+    Rm {
+        #[structopt(value_name = "KEY", help = "The key to remove")]
+        key: String,
+    },
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("get").about("Get value for key").arg(
-                Arg::with_name("key")
-                    .value_name("KEY")
-                    .help("The key to search for")
-                    .takes_value(true)
-                    .required(true),
-            ),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove value for key")
-                .arg(
-                    Arg::with_name("key")
-                        .value_name("KEY")
-                        .help("The key to search for")
-                        .takes_value(true)
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set value for key")
-                .arg(
-                    Arg::with_name("key")
-                        .value_name("KEY")
-                        .help("The key to search for")
-                        .takes_value(true)
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .value_name("VALUE")
-                        .help("The key to search for")
-                        .takes_value(true)
-                        .required(true),
-                ),
-        )
-        .get_matches();
 
-    match matches.subcommand() {
-        ("set", Some(_matches)) => {
+    match Opt::from_args() {
+        Opt::Set { key, value } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        ("get", Some(_matches)) => {
+        Opt::Rm { key } => {
             eprintln!("unimplemented");
             exit(1);
         }
-        ("rm", Some(_matches)) => {
+        Opt::Get { key } => {
             eprintln!("unimplemented");
             exit(1);
         }
+
         _ => unreachable!(),
     }
 }
